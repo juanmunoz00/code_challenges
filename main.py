f3b4e2b818ef80921bb729342d5e09fc9fb54877
juanmunoz00 <jmunoz@syner.info>
2023-10-18 18:10:08 +0000

    Merged sorted list done.

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ from tools import Tools
 
 Challenges = Challenges()  # Create an instance of the Challenges class
 
-## Reverse a string
-#list1 = ['1','2','3'] #For testing purposes
-hiMyName = "Hi my name is Juan"
-Challenges.Reverse_String(hiMyName)
+runChallenge = 2
+
+if (runChallenge == 1):
+  ## Reverse a string
+  #list1 = ['1','2','3'] #For testing purposes
+  hiMyName = "Hi my name is Juan"
+  Challenges.Reverse_String(hiMyName)
+
+if ( runChallenge == 2 ):
+  ## Merge two sorted arrays
+  list1 = [0,3,4,31]
+  list2 = [4,6,30]
+  Challenges.Merge_Sorted_Arrays(list1, list2)
--- misc_challenges.py
+++ misc_challenges.py
@@ -1,7 +1,45 @@
 from tools import Tools
 
 Tools = Tools() # Create an instance of the Tools class
+
 class Challenges:
   def Reverse_String(self, string):
+    # TODO: Lists validations
+    
     reversed_string = Tools.reverse_string(string)  # Call the method on the instance
     print(reversed_string)  # Print the result
+
+  def Merge_Sorted_Arrays(self, array1, array2):
+    # TODO: 
+    #  Add validations to the lists to make sure they are integers.
+    #  Add validation that if an element is repeated in the merged list, do not add it to the merges sorted list.
+    print("Given lists: ")
+    print(array1)
+    print(array2)
+        
+    mergedSortedArray = array1+array2
+    returnedMergedSortedArray = []
+    print("Merged list: ")
+    print(mergedSortedArray)    
+
+    i, j = 0, 0
+
+    try:
+      for k in range(len(mergedSortedArray)):
+          # Validates if the index i is less than the length of the array1
+          # If the validation is correct, the value of the array1 at index i is added to the returnedMergedSortedArray
+          if i < len(array1) and (j >= len(array2) or array1[i] < array2[j]):
+              returnedMergedSortedArray.append(array1[i])
+              i += 1
+          else:
+              returnedMergedSortedArray.append(array2[j])
+              j += 1
+  
+      print("Merged and sorted list: ")
+      print(returnedMergedSortedArray)
+      return returnedMergedSortedArray
+    except Exception as e: print(e)
+        
+
+      
+    
