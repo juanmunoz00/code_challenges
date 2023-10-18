21ffb1026f3699d22cf6755eb97ecc895f22ea0b
juanmunoz00 <jmunoz@syner.info>
2023-10-18 17:20:04 +0000

    Modify code so the challenges can use common algorithms.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from misc_challenges import Challenges
+from tools import Tools
 
-challenges_instance = Challenges()  # Create an instance of the Challenges class
+Challenges = Challenges()  # Create an instance of the Challenges class
 
 ## Reverse a string
 #list1 = ['1','2','3'] #For testing purposes
 hiMyName = "Hi my name is Juan"
-reversed_string = challenges_instance.reverse_string(hiMyName)  # Call the method on the instance
-print(reversed_string)  # Print the result
+Challenges.Reverse_String(hiMyName)
--- misc_challenges.py
+++ misc_challenges.py
@@ -1,3 +1,6 @@
+from tools import Tools
+
+Tools = Tools() # Create an instance of the Tools class
 class Challenges:
   """
   Reverse a string
@@ -9,17 +12,6 @@ class Challenges:
   Master the Coding Interview: Data Structures + Algorithms
   https://www.udemy.com/course/master-the-coding-interview-data-structures-algorithms/
   """
-  def reverse_string(self, string):
-    if not isinstance(string, str):
-      return "Input is not a string"
-    # Create an empty list named reversed. The list will be used to store the characters of the input string in reverse order.
-    reversed = []
-    
-    # The loop iterates over a range of numbers, starting from the length of the string minus one (the last index) and ending at -1. The -1 as the step size means that it will decrement the index with each iteration, effectively moving backward through the string.
-    
-    for i in range(len(string) - 1, -1, -1):      
-      # Each character of the input string is accessed by its index (starting from the end of the string), and it is added to the reversed list.
-      reversed.append(string[i])
-    
-    # The reversed list is converted to a string and returned.
-    return "".join(reversed)
+  def Reverse_String(self, string):
+    reversed_string = Tools.reverse_string(string)  # Call the method on the instance
+    print(reversed_string)  # Print the result
