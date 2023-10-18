adb72d3c15dfa5b3b0279049bd4ae6392810806d
juanmunoz00 <1782721-juanmunoz00@users.noreply.replit.com>
2023-10-18 16:46:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from misc_challenges import Challenges
+
+challenges_instance = Challenges()  # Create an instance of the Challenges class
+
+## Reverse a string
+hiMyName = "Hi My Name is Juan"
+reversed_string = challenges_instance.reverse_string(hiMyName)  # Call the method on the instance
+print(reversed_string)  # Print the result
---
+++ misc_challenges.py
@@ -0,0 +1,23 @@
+class Challenges:
+  """
+  Reverse a string
+
+  Last update: Oct. 2023
+
+  Description:
+  Challenge from the 
+  Master the Coding Interview: Data Structures + Algorithms
+  https://www.udemy.com/course/master-the-coding-interview-data-structures-algorithms/
+  """
+  def reverse_string(self, string):
+    # Create an empty list named reversed. The list will be used to store the characters of the input string in reverse order.
+    reversed = []
+    
+    # The loop iterates over a range of numbers, starting from the length of the string minus one (the last index) and ending at -1. The -1 as the step size means that it will decrement the index with each iteration, effectively moving backward through the string.
+    
+    for i in range(len(string) - 1, -1, -1):      
+      # Each character of the input string is accessed by its index (starting from the end of the string), and it is added to the reversed list.
+      reversed.append(string[i])
+    
+    # The reversed list is converted to a string and returned.
+    return "".join(reversed)
