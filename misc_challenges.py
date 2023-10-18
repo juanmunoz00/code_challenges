7bc30a9f78d0cb0c5573796ad1247b52b0ae68a5
juanmunoz00 <jmunoz@syner.info>
2023-10-18 17:27:42 +0000

    Added an index text file so the descriptions are written there and the code is cleaner.

--- misc_challenges.py
+++ misc_challenges.py
@@ -2,16 +2,6 @@ from tools import Tools
 
 Tools = Tools() # Create an instance of the Tools class
 class Challenges:
-  """
-  Reverse a string
-
-  Last update: Oct. 2023
-
-  Description:
-  Challenge from the 
-  Master the Coding Interview: Data Structures + Algorithms
-  https://www.udemy.com/course/master-the-coding-interview-data-structures-algorithms/
-  """
   def Reverse_String(self, string):
     reversed_string = Tools.reverse_string(string)  # Call the method on the instance
     print(reversed_string)  # Print the result
