2e522aa89219611cc749fcd231eaf3a88b4bdbea
juanmunoz00 <jmunoz@syner.info>
2023-10-18 19:50:24 +0000

    Find recurring character solved challenge added.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from functools import reduce
+import numpy as np
+
 from misc_challenges import Challenges
 from tools import Tools
 
-Challenges = Challenges()  # Create an instance of the Challenges class
+Challenges = Challenges()  # Create an instance of the Challenges class.
 
-runChallenge = 2
+runChallenge = Challenges.FIND_RECURRING_CHARACTER
 
 if (runChallenge == 1):
   ## Reverse a string
@@ -12,7 +15,16 @@ if (runChallenge == 1):
   Challenges.Reverse_String(hiMyName)
 
 if ( runChallenge == 2 ):
-  ## Merge two sorted arrays
+  ## Merge two sorted arrays into one sorted array.
   list1 = [0,3,4,31]
   list2 = [4,6,30]
   Challenges.Merge_Sorted_Arrays(list1, list2)
+
+if ( runChallenge == 3 ):
+  # Generates a 10-elements list of random integers between 20 and 40 as the input list.
+  num = 10
+  start = 20
+  end = 40
+  list1 = reduce(lambda acc, x: acc + [np.random.randint(start, end)], range(num), [])
+  
+  Challenges.Find_Recurring_Character(list1)
--- misc_challenges.py
+++ misc_challenges.py
@@ -3,11 +3,17 @@ from tools import Tools
 Tools = Tools() # Create an instance of the Tools class
 
 class Challenges:
+  REVERSE_STRING = 1
+  MERGED_SORTED_ARRAY = 2
+  FIND_RECURRING_CHARACTER = 3
+  
   def Reverse_String(self, string):
     # TODO: Lists validations
-    
-    reversed_string = Tools.reverse_string(string)  # Call the method on the instance
-    print(reversed_string)  # Print the result
+
+    try:
+      reversed_string = Tools.reverse_string(string)  # Call the method on the instance
+      print(reversed_string)  # Print the result
+    except Exception as e: print(e)
 
   def Merge_Sorted_Arrays(self, array1, array2):
     # TODO: 
@@ -39,7 +45,23 @@ class Challenges:
       print(returnedMergedSortedArray)
       return returnedMergedSortedArray
     except Exception as e: print(e)
-        
+
+  def Find_Recurring_Character(self, array):
+    print(array)
+    #Define the dictionaty where the list elements will be stored.
+    listDictionary = {}
+    try:
+      # Loop through the list and add each element to the dictionary.
+      for n in array:
+        # Since a dictionary cannot contain duplicate keys, if there's a recurrent key (repeated character) it will stop and exit the loop returning the recurrenct character.
+        if n in listDictionary:
+          print(f'The 1st recurring character is: ', str(n))
+          return n
+        else:
+          listDictionary[n] = str(n)
+      print(f'(Undefined) There is no recurring character in the list.'	)  
+    except Exception as e: print(e)
+  
 
       
     
